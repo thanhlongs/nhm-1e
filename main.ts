1bfe8e48e597334283016df22e402832f1dae9d3
thanhlongs <160575457+thanhlongs@users.noreply.github.com>
2024-02-20 13:38:55 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.lightLevel())
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(input.temperature())
+})
 basic.forever(function () {
 	
 })
